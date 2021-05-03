8b36277b892b5882e0476c296db281dd286f24eb
Ubuntu <ubuntu@ip-172-31-46-249.ap-south-1.compute.internal>
2021-05-03 08:46:24 +0000

    fav

--- Favourite-Service/src/main/java/com/stackroute/FavouriteService/repository/FavouriteRepository.java
+++ Favourite-Service/src/main/java/com/stackroute/FavouriteService/repository/FavouriteRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FavouriteRepository extends MongoRepository<Favourite,Integer>{
     @Query(value = "{'foodItem':$0}", delete = true)
     Favourite deleteByfoodItem(String foodItem);
     Favourite findByFoodItem(String foodItem);
     Boolean existsByFoodItem(String foodItem);
-    Favourite findByUsername(String username);
+    List<Favourite> findByUsername(String username);
 }

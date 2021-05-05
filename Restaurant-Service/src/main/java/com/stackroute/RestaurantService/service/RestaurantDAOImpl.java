0e380a6438dbd9fd5fe44532b9de6a6927379e17
Ubuntu <ubuntu@ip-172-31-36-43.ap-south-1.compute.internal>
2021-05-05 06:16:36 +0000

    code added

--- Restaurant-Service/src/main/java/com/stackroute/RestaurantService/service/RestaurantDAOImpl.java
+++ Restaurant-Service/src/main/java/com/stackroute/RestaurantService/service/RestaurantDAOImpl.java
@@ -7,24 +7,29 @@ import com.stackroute.RestaurantService.model.MenuItems;
 import com.stackroute.RestaurantService.model.Restaurant;
 import com.stackroute.RestaurantService.repository.RestaurantRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Slf4j
 @Service
 @AllArgsConstructor
 public class RestaurantDAOImpl implements RestaurantDAO {
     @Autowired
     private RestaurantRepository repository;
-
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(RestaurantDAOImpl.class);
     @Override
     public Restaurant addRestaurant(Restaurant restaurant)throws RestaurantAlreadyExistsException {
         if(repository.existsByName(restaurant.getName())){
+            logger.error("Restaurant already exists");
             throw new RestaurantAlreadyExistsException();
         }
         Restaurant restaurant1=repository.save(restaurant);
+        logger.info("New Restaurant added");
         return restaurant1;
     }
 
@@ -38,10 +43,12 @@ public class RestaurantDAOImpl implements RestaurantDAO {
         Restaurant restaurant=null;
         Optional optional=repository.findById(id);
         if(!optional.isPresent()) {
+            logger.error("Restaurant not found");
             throw new RestaurantNotFoundException();
         }
             restaurant=repository.findById(id).get();
             repository.deleteById(id);
+            logger.info("Restaurant deleted");
             return restaurant;
 
     }
@@ -51,12 +58,14 @@ public class RestaurantDAOImpl implements RestaurantDAO {
         Restaurant updatedRestaurant=null;
         //Optional optional=Optional.of(repository.findById(restaurant.getRestaurantId()));
         if(!repository.existsByName(restaurant.getName())) {
+            logger.error("Restaurant not found");
             throw new RestaurantNotFoundException();
         }
             /* Restaurant getRestaurant=repository.findById(restaurant.getRestaurantId()).get();
             getRestaurant.setName(restaurant.getName());
             getRestaurant.setLocation(restaurant.getLocation());*/
             updatedRestaurant=repository.save(restaurant);
+            logger.error("Restaurant updated");
             return updatedRestaurant;
     }
 
@@ -66,9 +75,11 @@ public class RestaurantDAOImpl implements RestaurantDAO {
     public Restaurant findByLocation(String location) throws RestaurantNotFoundException{
        //Optional optional=Optional.of(repository.findByLocation(location));
        if(!repository.existsByLocation(location)){
+           logger.error("Restaurant not found");
            throw new RestaurantNotFoundException();
        }
        Restaurant restaurant=repository.findByLocation(location);
+       logger.info("Restaurant fetched based on location");
        return restaurant;
     }
 
@@ -76,9 +87,11 @@ public class RestaurantDAOImpl implements RestaurantDAO {
     public Restaurant findByName(String name) throws RestaurantNotFoundException {
        // Optional optional=Optional.of(repository.findByName(name));
         if(!repository.existsByName(name)){
+            logger.error("Restaurant not found ");
             throw  new RestaurantNotFoundException();
         }
         Restaurant restaurant=repository.findByName(name);
+        logger.info("Restaurant  fetched based on name");
         return restaurant;
     }
 
@@ -86,9 +99,11 @@ public class RestaurantDAOImpl implements RestaurantDAO {
     public List<Restaurant> searchByMenuItems(String menuItem) throws MenuItemNotFoundException {
         Optional optional=Optional.of(repository.findByMenuItem(menuItem));
         if(!optional.isPresent()){
+            logger.error("Restaurant not found");
             throw new MenuItemNotFoundException();
         }
         List<Restaurant> restaurant=repository.findByMenuItem(menuItem);
+        logger.info("Restaurant fetched based on menu item");
         return restaurant;
     }
 }
